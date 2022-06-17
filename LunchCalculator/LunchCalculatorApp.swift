b7ced457805184741285e83a2e9e37663ffcb025
Brett Koster <skdfmanagement@icloud.com>
2022-06-17 15:06:57 -0500

    Initial Commit

---
+++ LunchCalculator/LunchCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchCalculatorApp.swift
+//  LunchCalculator
+//
+//  Created by Brett Koster on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LunchCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
